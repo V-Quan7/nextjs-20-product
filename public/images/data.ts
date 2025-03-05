c11537498b23fc0213fceb63dcb337ada14464b2
Van Quan <quanpham150700@gmail.com>
2025-03-05 15:25:07 +0700

    uppdate

---
+++ public/images/data.ts
@@ -0,0 +1,10 @@
+const data = {
+    carousels:
+        [
+            title: 'Most Popular Shoes For Sale',
+            buttonCaption: 'Shop Now',
+            image: '/images/banner3.jpg',
+            url: '/search?category=Shoes',
+            isPublished: true,
+        ]
+}
